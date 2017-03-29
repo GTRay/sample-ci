30e540f4df7e180746e97701bcbb7400324ea891
Ruizhi Chai <rchai3@gatech.edu>
2017-03-29 16:09:47 -0400

    fix the build

--- js/book.controller.spec.js
+++ js/book.controller.spec.js
@@ -15,8 +15,8 @@ describe('Book controller', function() {
     expect(3+2).toEqual(5);
   });
   it('has a dummy spec to test 3 + 3', function() {
-	  expect(3+3).toEqual(12);
-      });
+    expect(3+3).toEqual(6);
+  });
 
   describe('$scope.name', function() {
     it('names the controller', function() {
